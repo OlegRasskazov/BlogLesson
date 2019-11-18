73b6e985c1bf4388b202db666d606c8854bc3833
Rasskazov Oleg <rasskazov_oe@quorum.local>
2019-11-18 19:54:56 +0300

    step 7 of lesson

--- Blog/Data/AppDbContext.cs
+++ Blog/Data/AppDbContext.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Blog.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Data
 {
-    public class AppDbContext: DbContext
+    public class AppDbContext: IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options)
         :base(options)
--- Blog/Program.cs
+++ Blog/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Blog
@@ -14,7 +17,47 @@ namespace Blog
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            try
+            {
+
+                var scope = host.Services.CreateScope();
+
+                var ctx = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                var usetManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                ctx.Database.EnsureCreated();
+
+                var adminRole = new IdentityRole("Admin");
+
+                if (!ctx.Roles.Any())
+                {
+                    //create role
+                    roleManager.CreateAsync(adminRole).GetAwaiter().GetResult();
+                }
+
+                if (!ctx.Users.Any(u => u.UserName == "admin"))
+                {
+                    //create an admin
+                    var adminUser = new IdentityUser
+                    {
+                        UserName = "admin",
+                        Email = "admin@test.com "
+                    };
+                    usetManager.CreateAsync(adminUser, "password").GetAwaiter().GetResult();
+                    // add role to user
+                    usetManager.AddToRoleAsync(adminUser, adminRole.Name).GetAwaiter().GetResult();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
